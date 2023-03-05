b52f7fd2aa52250f8c8b6e3de51a0cd3f7c6ebb9
Norbert Merkli <norbertmerkli11@gmail.com>
2023-03-05 16:16:17 +0100

    Add custom fonts to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Libre Baskerville", ...fontFamily.serif],
+        sans: ["Nunito", ...fontFamily.sans],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
