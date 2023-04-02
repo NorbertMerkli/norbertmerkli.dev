34bc84a08174c8fb7a9f1f753ee7f1845ccb5d36
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-02 11:28:52 +0200

    Add custom color-swatches to 'tailwind.config.js'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,6 +9,24 @@ module.exports = {
       fontFamily: {
         serif: ["Libre Baskerville", ...fontFamily.serif],
         sans: ["Nunito", ...fontFamily.sans],
+      },
+      colors: {
+        primary: colors.sky[500],
+        secondary: colors.sky[200],
+        black: colors.slate[700],
+        darkgray: colors.slate[400],
+        lightgray: colors.slate[300],
+        white: colors.sky[50],
+        gradient: {
+          start: {
+            light: colors.sky[100],
+            dark: colors.slate[800],
+          },
+          end: {
+            light: colors.slate[100],
+            dark: colors.slate[700],
+          },
+        },
       },
     },
   },
