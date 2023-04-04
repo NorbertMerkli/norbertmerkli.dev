209ba64f5e505764ea64e921f3aae33ef3917c14
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-04 14:40:34 +0200

    Add base styles for text and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import heroIllustration from "@public/hero-illustration.svg";
 
 export default function Home() {
   return (
-    <section className="page-section bg-gradient">
+    <section className="page-section bg-gradient h-screen">
       <h1>
         I&apos;m <span className="text-primary">Norbert</span> Merkli
       </h1>
-      <h2>A full-stack web developer.</h2>
+      <h2 className="text-darkgray dark:text-white">
+        A full-stack web developer.
+      </h2>
       <p className=" my-8 text-center text-xl">
         Welcome to my site. Here I showcase my skills and share what I&apos;m
         learning about different technologies.
