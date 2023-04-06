68cac0762af7e53ff1bd1ab79dea1b31f4a92791
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-06 11:19:05 +0200

    Move base styles to the global css file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="overflow-hidden">
-      <body className="bg-gradient h-screen overflow-x-hidden overflow-y-scroll">
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
