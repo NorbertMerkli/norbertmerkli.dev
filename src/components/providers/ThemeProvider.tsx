a87245d3cc2cc8726a0fa7c45576b92c43f73d6f
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-06 21:56:16 +0200

    Implement a color-theme provider

---
+++ src/components/providers/ThemeProvider.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { PropsWithChildren, createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext({});
+
+export type ThemeContextType = {
+  isDark: boolean;
+  switchTheme: () => void;
+};
+
+function getTheme(): boolean {
+  const lastTheme = localStorage.theme;
+
+  const darkTheme =
+    lastTheme === undefined
+      ? window.matchMedia("(prefers-color-scheme: dark)").matches
+      : lastTheme === "dark";
+
+  darkTheme
+    ? document.documentElement.classList.add("dark")
+    : document.documentElement.classList.remove("dark");
+
+  return darkTheme;
+}
+
+export const ThemeProvider = ({ children }: PropsWithChildren) => {
+  const [isDark, setDark] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    setDark(getTheme());
+  }, []);
+
+  const switchTheme = () =>
+    setDark((lastValue) => {
+      const newValue = !lastValue;
+
+      if (newValue) {
+        localStorage.theme = "dark";
+        document.documentElement.classList.add("dark");
+      } else {
+        localStorage.theme = "light";
+        document.documentElement.classList.remove("dark");
+      }
+
+      return newValue;
+    });
+
+  return (
+    <ThemeContext.Provider value={{ isDark, switchTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
