8b218f4889e8ea9c01b77295fa8b41ec3761f990
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-09 08:31:54 +0200

    Make prop-type definitions more precise for icon components

--- src/components/svg/index.tsx
+++ src/components/svg/index.tsx
@@ -2,17 +2,20 @@ import { EventHandler, MouseEvent } from "react";
 
 import * as Icons from "./icons";
 
-export type IconName = keyof typeof Icons;
+type IconName = keyof typeof Icons;
+type IconProps = { icon: IconName };
+type IconButtonProps = IconProps & { onClick: EventHandler<MouseEvent> };
+type IconLinkProps = IconProps & { href: string };
 
-type IconProps = {
-  icon: IconName;
-  onClick?: EventHandler<MouseEvent>;
-  href?: string;
+export const PassiveIcon = ({ icon }: IconProps) => {
+  const IconComponent = Icons[icon];
+
+  return (
+    <div className="passive-icon">
+      <IconComponent />
+    </div>
+  );
 };
-
-type IconButtonProps = Omit<IconProps, "href">;
-type IconLinkProps = Omit<IconProps, "onClick">;
-type PassiveIconProps = Omit<IconProps, "href" | "onClick">;
 
 export const IconButton = ({ icon, onClick }: IconButtonProps) => {
   const IconComponent = Icons[icon];
@@ -31,15 +34,5 @@ export const IconLink = ({ icon, href }: IconLinkProps) => {
     <a href={href} className="icon-link">
       <IconComponent />
     </a>
-  );
-};
-
-export const PassiveIcon = ({ icon }: PassiveIconProps) => {
-  const IconComponent = Icons[icon];
-
-  return (
-    <div className="passive-icon">
-      <IconComponent />
-    </div>
   );
 };
