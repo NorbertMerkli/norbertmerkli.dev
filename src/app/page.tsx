3a090738455cf4137b05fb7e7694590d8a2b1f79
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-09 16:08:16 +0200

    Align background-gradient's top to the bottom of the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { IconLink } from "@components/svg";
 export default function Home() {
   return (
     <>
-      <header className="safe-area flex h-screen flex-col gap-8 py-20 lg:flex-row lg:gap-16">
+      <header className="safe-area flex h-screen flex-col gap-8 pb-20 pt-6 lg:flex-row lg:gap-16">
         <section className="flex-none self-center text-center lg:flex-1">
           <h1 className="font-serif text-6xl sm:text-7xl lg:text-left">
             I&apos;m <span className="text-primary">Norbert</span> Merkli
