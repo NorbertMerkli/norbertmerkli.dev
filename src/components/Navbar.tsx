2f74d0e31dfbc4ebed592d293d09712cbf4b26f2
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-09 11:45:48 +0200

    Make the navbar design more cohesive with the page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { ThemeSwitch } from "@components/ThemeSwitch";
 
 export const Navbar = () => (
-  <nav className="fixed top-0 flex h-14 w-full flex-row items-center justify-between px-2">
-    <ThemeSwitch />
-  </nav>
+  <div className="fixed top-0 z-50 w-full border-b border-lightgray bg-gradient-end-light px-2 shadow-md shadow-lightgray/50 dark:border-gradient-start-dark dark:bg-gradient-end-dark dark:shadow-gradient-start-dark/50">
+    <nav className="safe-area flex h-14 flex-row items-center justify-between">
+      <ThemeSwitch />
+    </nav>
+  </div>
 );
