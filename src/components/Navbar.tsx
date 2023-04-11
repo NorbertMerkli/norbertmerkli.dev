b7fb7dfc9b9cf56cea3ee054b2e729ffe00d209c
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-11 11:47:36 +0200

    Remove unnecessary extra padding from the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { ThemeSwitch } from "@components/ThemeSwitch";
 
 export const Navbar = () => (
-  <div className="fixed top-0 z-50 w-full border-b border-lightgray bg-gradient-end-light px-2 shadow-md shadow-lightgray/50 dark:border-gradient-start-dark dark:bg-gradient-end-dark dark:shadow-gradient-start-dark/50">
+  <div className="fixed top-0 z-50 w-full border-b border-lightgray bg-gradient-end-light shadow-md shadow-lightgray/50 dark:border-gradient-start-dark dark:bg-gradient-end-dark dark:shadow-gradient-start-dark/50">
     <nav className="safe-area flex h-14 flex-row items-center justify-between">
       <ThemeSwitch />
     </nav>
