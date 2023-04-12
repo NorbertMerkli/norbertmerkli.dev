6963b68ec10f5e861cf54daf26656db1416a6a74
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-12 09:24:37 +0200

    Give a raised look to the navbar on page-scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,32 @@
-import { ThemeSwitch } from "@components/ThemeSwitch";
+"use client";
 
-export const Navbar = () => (
-  <div className="fixed top-0 z-50 w-full border-b border-lightgray bg-gradient-end-light shadow-md shadow-lightgray/50 dark:border-gradient-start-dark dark:bg-gradient-end-dark dark:shadow-gradient-start-dark/50">
-    <nav className="safe-area flex h-14 flex-row items-center justify-between">
-      <ThemeSwitch />
-    </nav>
-  </div>
-);
+import { ThemeSwitch } from "@components/ThemeSwitch";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useState } from "react";
+
+export const Navbar = () => {
+  const scrollThreshold = 20;
+
+  const { scrollY } = useScroll();
+  const [isPageStart, setIsPageStart] = useState(scrollY.get() < 40);
+
+  useMotionValueEvent(scrollY, "change", (value) => {
+    if (scrollY.get() < scrollThreshold && !isPageStart) setIsPageStart(true);
+    if (scrollY.get() > scrollThreshold && isPageStart) setIsPageStart(false);
+  });
+
+  return (
+    <div
+      className={
+        "fixed top-0 z-50 w-full border-b border-lightgray shadow-md shadow-lightgray/50 duration-100 dark:border-gradient-start-dark dark:shadow-gradient-start-dark/50" +
+        (isPageStart
+          ? " border-none bg-transparent shadow-none"
+          : " border-b bg-gradient-end-light shadow-md dark:bg-gradient-end-dark")
+      }
+    >
+      <nav className="safe-area flex h-14 flex-row items-center justify-between">
+        <ThemeSwitch />
+      </nav>
+    </div>
+  );
+};
