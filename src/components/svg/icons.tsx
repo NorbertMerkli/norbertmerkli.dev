4f3b346f5eeb35c28202bb3edf8472fece613561
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-16 21:09:01 +0200

    Add up arrow icon

--- src/components/svg/icons.tsx
+++ src/components/svg/icons.tsx
@@ -216,3 +216,17 @@ export const LightMode = () => (
     />
   </svg>
 );
+
+export const UpArrow = () => (
+  <svg
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    className="h-6 w-6 fill-none stroke-white"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.5 15.75l7.5-7.5 7.5 7.5"
+    />
+  </svg>
+);
