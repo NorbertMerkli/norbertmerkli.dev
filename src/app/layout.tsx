b875768b9901fcad890f9c974cfca754a45b6937
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-18 19:49:44 +0200

    Add detailed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,31 @@ import "@styles/globals.css";
 import { ThemeProvider } from "@components/providers/ThemeProvider";
 import { Navbar } from "@components/Navbar";
 
+const title = "Norbert Merkli";
+const description = "Full-stack web developer.";
+const ogImage = "https://norbertmerkli.dev/og";
+
 export const metadata = {
   title: "norbertmerkli.dev",
-  description: "Dev portfolio",
+  description: "My developer portfolio",
+  openGraph: {
+    title,
+    description,
+    type: "website",
+    url: "https://norbertmerkli.dev",
+    images: {
+      url: ogImage,
+      alt: "Personal logo",
+    },
+    siteName: "norbertmerkli.dev",
+  },
+  twitter: {
+    title,
+    description,
+    site: "@Norbert_Merkli",
+    card: "summary_large_image",
+    images: [ogImage],
+  },
 };
 
 export default function RootLayout({
