c0a6534ba2927d8c16abfaa62ac5f65230557403
Norbert Merkli <norbertmerkli11@gmail.com>
2023-04-19 17:54:25 +0200

    Add type explicitly to the metadata object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import "@styles/globals.css";
 
 import { ThemeProvider } from "@components/providers/ThemeProvider";
 import { Navbar } from "@components/Navbar";
+import { Metadata } from "next";
 
 const title = "Norbert Merkli";
 const description = "Full-stack web developer.";
 const ogImage = "https://norbertmerkli.dev/og";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "norbertmerkli.dev",
   description: "My developer portfolio",
   openGraph: {
